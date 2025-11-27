a5ca57123f215e9928fe2be8606ba748d3f0d37d
Denish <denishbhuva01@gmail.com>
2025-11-27 15:08:22 +0530

    input prog

---
+++ guessing_game/guess_game/src/main.rs
@@ -0,0 +1,13 @@
+use std::io;
+
+fn main() {
+    println!("Type something:");
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    println!("You typed: {}", input);
+}
