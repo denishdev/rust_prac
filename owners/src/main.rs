1b6af10fb7a7b4bffaa4ead9eda8da3f52904b6a
Denish <denishbhuva01@gmail.com>
2025-11-29 21:35:26 +0530

    some ref

--- owners/src/main.rs
+++ owners/src/main.rs
@@ -1,6 +1,16 @@
 fn main() {
-    println!("Hello, world!");
-    let s1 = String::from("i'm owner");
-    let s2 = s1;
-    println!("{}",s2);
+    // println!("Hello, world!");
+    // let s1 = String::from("i'm owner");
+    // let s2 = s1;
+    // println!("{}",s2);
+
+    //Reference
+
+    let mut s1 = String::from("Hello");
+    append_str(&mut s1);
+    println!("{}",s1);
+}
+
+fn append_str(s: &mut String){
+    s.push_str("world");
 }
